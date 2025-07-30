1fa327e41c5e34e774d337a775cd43cc86ad7f4d
JeanyAurellia <jeanyaurellia@gmail.com>
2025-07-30 11:00:13 +0700

    modified:   src/components/Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,6 +8,6 @@
       <li><a href="#services">Services</a></li>
       <li><a href="#project">My Project</a></li>
     </ul>
-    <button class="bg-indigo-500 text-white px-4 py-2 rounded-md text-sm">Contact Us</button>
+    <a href="#contact" class="bg-indigo-500 text-white px-4 py-2 rounded-md text-sm">Contact Me</a>
   </nav>
 </template>
